2c6d96c0c08bd7779ccdf8ab4f16f15b253f923d
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-29 17:41:31 -0500

    solution

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,35 @@
 require_relative '../../db/config'
+# require 'csv'
+# require 'date'
 
 # implement your Student model here
+class Student < ActiveRecord::Base
+
+
+  
+
+  validates :email, :uniqueness => true, :format => { :with => /\S+@\S+[.].{2,}/,
+    :message => "Incorrect email format" }
+
+  validate :not_a_toddler
+
+  validates :phone, :format => { :with => /\d{3}\D*\d{3}\D*\d{4}/,
+    :message => "Not a phone number!!!" }
+
+  
+
+  def name
+    first_name + " " + last_name
+  end
+
+  def age
+    now = Date.today
+    now.year - self.birthday.year - ((now.month > self.birthday.month || (now.month == self.birthday.month && now.day >= self.birthday.day)) ? 0 : 1)
+  end
+
+  def not_a_toddler
+    if age < 5
+      errors.add(:birthday, "This baby is too young")
+    end
+  end
+end
